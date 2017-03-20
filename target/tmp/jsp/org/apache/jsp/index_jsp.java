418feb3bb6106fc53c2b152b13267d5f3e5cc9b3
Student <student@student.pl>
2017-03-20 23:49:54 +0100

    target folder?

--- target/tmp/jsp/org/apache/jsp/index_jsp.java
+++ target/tmp/jsp/org/apache/jsp/index_jsp.java
@@ -11,10 +11,20 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.Vector _jspx_dependants;
 
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_if_test;
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public Object getDependants() {
     return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_if_test = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_if_test.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -41,44 +51,65 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write("\n");
-      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
-      out.write("<html>\n");
-      out.write("<head>\n");
-      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
-      out.write("<title>Hello World</title>\n");
-      out.write("</head>\n");
-      out.write("<body>\n");
-      out.write("<form action=\"hello\" method=\"post\">\n");
-      out.write("<table>\n");
-      out.write("<tr>\n");
-      out.write("<td><label>Kwota kredytu:</label></td>\n");
-      out.write("<td><input type=\"number\" name=\"amount\"></td>\n");
-      out.write("</tr>\n");
-      out.write("<tr>\n");
-      out.write("<td><label>Ilość rat:</label></td>\n");
-      out.write("<td><input type=\"number\" name=\"rates_count\"></td>\n");
-      out.write("</tr>\n");
-      out.write("<tr>\n");
-      out.write("<td><label>Oprocentowanie:</label></td>\n");
-      out.write("<td><input type=\"number\" name=\"percent\"></td>\n");
-      out.write("</tr>\n");
-      out.write("<tr>\n");
-      out.write("<td><label>Opłata stała:</label></td>\n");
-      out.write("<td><input type=\"number\" name=\"const_rate_amount\"></td>\n");
-      out.write("</tr>\n");
-      out.write("<tr>\n");
-      out.write("<td><label>Rodzaj rat:</label></td>\n");
-      out.write("<td>\n");
-      out.write("<input type=\"radio\" name=\"rates_type\" value=\"decrease\">Malejące\n");
-      out.write("<input type=\"radio\" name=\"rates_type\" value=\"const\">Stałe\n");
-      out.write("</td>\n");
-      out.write("</tr>\n");
-      out.write("</table>\n");
-      out.write("<br/>\n");
-      out.write("<input type=\"submit\" value=\"Harmonogram spłat >\">\n");
-      out.write("</form>\n");
-      out.write("</body>\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.min.css\">\r\n");
+      out.write("    <title>Kalkulator raty kredytu</title>\r\n");
+      out.write("</head>\r\n");
+      out.write("<body>\r\n");
+      out.write("<div class=\"container\" style=\"padding: 30px;\">\r\n");
+      out.write("    <div class=\"row\">\r\n");
+      out.write("        <div class=\"col-md-6 col-md-offset-3\">\r\n");
+      out.write("            <form action=\"calculate\" method=\"post\">\r\n");
+      out.write("                ");
+      if (_jspx_meth_c_if_0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("                ");
+ session.removeAttribute("FormError"); 
+      out.write("\r\n");
+      out.write("                <table class=\"table\">\r\n");
+      out.write("                    <tr>\r\n");
+      out.write("                        <td width=\"40%\"><label>Kwota kredytu:</label></td>\r\n");
+      out.write("                        <td><input type=\"number\" class=\"form-control\" min=\"0\" name=\"amount\"></td>\r\n");
+      out.write("                    </tr>\r\n");
+      out.write("                    <tr>\r\n");
+      out.write("                        <td><label>Ilość rat:</label></td>\r\n");
+      out.write("                        <td><input type=\"number\" class=\"form-control\" min=\"0\" name=\"rates_count\"></td>\r\n");
+      out.write("                    </tr>\r\n");
+      out.write("                    <tr>\r\n");
+      out.write("                        <td><label>Oprocentowanie:</label></td>\r\n");
+      out.write("                        <td><input type=\"number\" class=\"form-control\" min=\"0\" step=\"0.01\" name=\"percent\"></td>\r\n");
+      out.write("                    </tr>\r\n");
+      out.write("                    ");
+      out.write("\r\n");
+      out.write("                    ");
+      out.write("\r\n");
+      out.write("                        ");
+      out.write("\r\n");
+      out.write("                        ");
+      out.write("\r\n");
+      out.write("                    ");
+      out.write("\r\n");
+      out.write("                    <tr>\r\n");
+      out.write("                        <td><label>Rodzaj rat:</label></td>\r\n");
+      out.write("                        <td>\r\n");
+      out.write("                            <div class=\"form-check\"><input type=\"radio\" class=\"form-check-input\" name=\"rates_type\" value=\"decrease\"> Malejące</div>\r\n");
+      out.write("                            <div class=\"form-check\"><input type=\"radio\" class=\"form-check-input\" name=\"rates_type\" value=\"equal\"> Stałe</div>\r\n");
+      out.write("                        </td>\r\n");
+      out.write("                    </tr>\r\n");
+      out.write("                </table>\r\n");
+      out.write("                <br/>\r\n");
+      out.write("                <input type=\"submit\" class=\"btn btn-primary pull-right\" value=\"Harmonogram spłat\">\r\n");
+      out.write("            </form>\r\n");
+      out.write("        </div>\r\n");
+      out.write("    </div>\r\n");
+      out.write("</div>\r\n");
+      out.write("</body>\r\n");
       out.write("</html>");
     } catch (Throwable t) {
       if (!(t instanceof SkipPageException)){
@@ -91,5 +122,33 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
     }
+  }
+
+  private boolean _jspx_meth_c_if_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:if
+    org.apache.taglibs.standard.tag.rt.core.IfTag _jspx_th_c_if_0 = (org.apache.taglibs.standard.tag.rt.core.IfTag) _jspx_tagPool_c_if_test.get(org.apache.taglibs.standard.tag.rt.core.IfTag.class);
+    _jspx_th_c_if_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_if_0.setParent(null);
+    _jspx_th_c_if_0.setTest(((java.lang.Boolean) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${sessionScope.FormError}", java.lang.Boolean.class, (PageContext)_jspx_page_context, null)).booleanValue());
+    int _jspx_eval_c_if_0 = _jspx_th_c_if_0.doStartTag();
+    if (_jspx_eval_c_if_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\r\n");
+        out.write("                    <div class=\"alert alert-danger\">Podaj poprawne dane</div>\r\n");
+        out.write("                ");
+        int evalDoAfterBody = _jspx_th_c_if_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_if_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+      return true;
+    }
+    _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+    return false;
   }
 }
